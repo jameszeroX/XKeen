ce666676c346303dbaec00dcbf1d722fbf7b7a34
jameszeroX <145020465+jameszeroX@users.noreply.github.com>
2025-06-04 15:19:51 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
 opkg install tar
-curl -s -L https://github.com/jameszeroX/XKeen/releases/latest/download/xkeen.tar --output xkeen.tar && tar -xvf xkeen.tar -C /opt/sbin --overwrite > /dev/null && rm xkeen.tar
+curl -L https://github.com/jameszeroX/XKeen/releases/latest/download/xkeen.tar -o xkeen.tar
+tar -xvf xkeen.tar -C /opt/sbin --overwrite > /dev/null && rm xkeen.tar
 xkeen -i
