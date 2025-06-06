3c8fc73e8bb4c29896d5f244e7fd160032f7e4f6
jameszeroX <145020465+jameszeroX@users.noreply.github.com>
2025-06-06 08:49:57 +0300

    Delete scripts directory

--- scripts/_xkeen/04_tools/02_tools_choose/04_choose_cron/00_cron_import.sh
+++
@@ -1,5 +0,0 @@
-# Импорт модулей вопросов cron
-
-# Модули вопросов cron
-. "$xtools_dir/02_tools_choose/04_choose_cron/01_cron_status.sh"
-. "$xtools_dir/02_tools_choose/04_choose_cron/02_cron_time.sh"
