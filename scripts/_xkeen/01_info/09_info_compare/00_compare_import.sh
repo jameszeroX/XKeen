843e05a69512b1942acb7545365db4dc51bee388
jameszeroX <admin@jameszero.net>
2025-06-07 09:32:17 +0300

    Update

--- scripts/_xkeen/01_info/09_info_compare/00_compare_import.sh
+++
@@ -1,5 +0,0 @@
-# Импорт модулей сравнения версий
-
-# Модули сравнения версий
-. "$xinfo_dir/09_info_compare/01_compare_xkeen.sh"
-. "$xinfo_dir/09_info_compare/02_compare_xray.sh"
--- scripts/_xkeen/03_delete/04_delete_configs.sh
+++
@@ -1,5 +0,0 @@
-# Удаление всех конфигураций xray
-
-delete_configs() {
-    find "$install_conf_dir" -name "*.json" -type f -delete
-}
