b56fc1868ab404f148614d0d363b8461dedf48a2
Andrey Paramonov <a.paramonov@dodopizza.com>
2025-06-22 02:29:01 +0300

    Refactor tar extraction command for improved readability in installation script

--- install.sh
+++ install.sh
@@ -1,5 +1,3 @@
 curl -OL https://github.com/jameszeroX/XKeen/releases/latest/download/xkeen.tar.gz
-c="tar -xvzf xkeen.tar.gz -C /opt/sbin >/dev/null 2>&1"
-$c --overwrite || $c
-rm xkeen.tar.gz
+c="tar -xvzf xkeen.tar.gz -C /opt/sbin >/dev/null 2>&1";$c --overwrite || $c && rm xkeen.tar.gz
 xkeen -i
