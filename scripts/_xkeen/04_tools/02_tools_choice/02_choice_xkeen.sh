512a39d87f35daec78f76b5aab99eaf45f1fe207
jameszeroX <admin@jameszero.net>
2025-07-11 20:26:04 +0300

    1.1.3.8 Preview

--- scripts/_xkeen/04_tools/02_tools_choice/02_choice_xkeen.sh
+++ scripts/_xkeen/04_tools/02_tools_choice/02_choice_xkeen.sh
@@ -2,7 +2,7 @@
 choice_channel_xkeen() {
     if [ "$xkeen_build" = "Stable" ]; then
         echo -e "  Текущий канал обновлений ${yellow}XKeen${reset}: Стабильная версия (${green}Stable${reset})"
-        read -p "  Хотите переключиться на канал разработки? (y/N) либо (д/Н): " choice
+        read -r -p "  Хотите переключиться на канал разработки? (y/N) либо (д/Н): " choice
         if echo "$choice" | grep -iq "^[yд]"; then
             choice_build="Dev"
         else
@@ -10,7 +10,7 @@ choice_channel_xkeen() {
         fi
     else
         echo -e "  Текущий канал обновлений ${yellow}XKeen${reset}: Версия в разработке (${green}$xkeen_build${reset})"
-        read -p "  Хотите переключиться на канал стабильной версии? (y/N) либо (д/Н): " choice
+        read -r -p "  Хотите переключиться на канал стабильной версии? (y/N) либо (д/Н): " choice
         if echo "$choice" | grep -iq "^[yд]"; then
             choice_build="Stable"
         else
