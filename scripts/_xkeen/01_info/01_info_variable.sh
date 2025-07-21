7bbe0d4e8858f3d1430766d6060a211704068444
jameszeroX <admin@jameszero.net>
2025-07-21 20:19:47 +0300

    1.1.3.8 Beta

--- scripts/_xkeen/01_info/01_info_variable.sh
+++ scripts/_xkeen/01_info/01_info_variable.sh
@@ -36,7 +36,7 @@ os_modules="/lib/modules/$(uname -r)"
 user_modules="/opt/lib/modules"
 app_name=XKeen
 xkeen_current_version="1.1.3.8"
-xkeen_build="Preview"
+xkeen_build="Beta"
 
 # -------------------------------------
 # Время
