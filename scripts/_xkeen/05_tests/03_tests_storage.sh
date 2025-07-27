57a31324f778f3d8b7feb3bd869028ce57f31643
jameszeroX <admin@jameszero.net>
2025-07-27 14:39:00 +0300

    1.1.3.8 Beta

--- scripts/_xkeen/05_tests/03_tests_storage.sh
+++ scripts/_xkeen/05_tests/03_tests_storage.sh
@@ -14,34 +14,34 @@ tests_entware_storage() {
 }
 
 preinstall_warn() {
-        if [ -n "$preinstall_warn" ]; then
-            echo
-            echo -e "  ${red}Внимание${reset}: Инициирована установка XKeen $entware_storage"
-            echo "  Убедитесь, что на ней достаточно свободного места. Сбой при такой"
-            echo "  установке не является проблемой XKeen и багрепорт не будет рассмотрен"
-            echo -e "  XKeen ${green}рекомендуется${reset} устанавливать на внешний ${green}USB-накопитель${reset}"
-            echo
-            echo "  1. Продолжить установку $entware_storage"
-            echo "  2. Выйти из установщика"
-            echo
+    if [ -n "$preinstall_warn" ]; then
+        echo
+        echo -e "  ${red}Внимание${reset}: Инициирована установка XKeen $entware_storage"
+        echo "  Убедитесь, что на ней достаточно свободного места. Сбой при такой"
+        echo "  установке не является проблемой XKeen и багрепорт не будет рассмотрен"
+        echo -e "  XKeen ${green}рекомендуется${reset} устанавливать на внешний ${green}USB-накопитель${reset}"
+        echo
+        echo "  1. Продолжить установку $entware_storage"
+        echo "  2. Выйти из установщика"
+        echo
 
-        while true; do
-            read -p "  Выберите действие: " choice
+    while true; do
+        read -p "  Выберите действие: " choice
 
-            case $choice in
-                1)
-                    clear
-                    break
-                    ;;
-                2)
-                    echo
-                    echo -e "  ${red}Установка отменена${reset}"
-                    exit 0
-                    ;;
-                *)
-                    echo -e "  ${red}Некорректный ввод.${reset} Выберите один из предложенных вариантов"
-                    ;;
-            esac
-        done
-        fi
+        case $choice in
+            1)
+                clear
+                break
+                ;;
+            2)
+                echo
+                echo -e "  ${red}Установка отменена${reset}"
+                exit 0
+                ;;
+            *)
+                echo -e "  ${red}Некорректный ввод.${reset} Выберите один из предложенных вариантов"
+                ;;
+        esac
+    done
+    fi
 }
