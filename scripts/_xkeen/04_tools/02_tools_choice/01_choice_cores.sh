a52fdd3cac2b3edb94ad08ee047b13bce82904b2
jameszeroX <admin@jameszero.net>
2025-08-03 12:05:19 +0300

    1.1.3.8 Beta

--- scripts/_xkeen/04_tools/02_tools_choice/01_choice_cores.sh
+++ scripts/_xkeen/04_tools/02_tools_choice/01_choice_cores.sh
@@ -50,3 +50,42 @@ choice_add_proxy_cores() {
         [ "$valid_input" = "true" ] && break
     done
 }
+
+# Смена ядра проксирования на Xray
+choice_xray_core() {  
+    command -v xray >/dev/null 2>&1 || { echo -e "  ${red}Ошибка${reset}: Ядро Xray не установлено. Выполните установку командой ${yellow}xkeen -ux${reset}"; exit 1; }
+    if grep -q 'name_client="xray"' $initd_dir/S24xray; then
+        echo -e " Смена ядра ${red}не выполнена${reset}. Устройство уже работает на ядре ${yellow}Xray${reset}"
+    elif grep -q 'name_client="mihomo"' $initd_dir/S24xray; then
+        if pidof "mihomo" >/dev/null; then
+            $initd_dir/S24xray stop
+        fi
+        sed -i 's/name_client="mihomo"/name_client="xray"/' $initd_dir/S24xray
+        add_chmod_init
+        echo -e "  ${green}Выполнена${reset} смена ядра на ${yellow}Xray${reset}"
+        echo -e "  Настройте конфигурацию по пути '${yellow}$install_conf_dir/${reset}'"
+        echo -e "  И запустите проксирование командой ${yellow}xkeen -start${reset}"
+    else
+        echo -e " Произошла ${red}ошибка${reset} при смене ядра проксирования"
+    fi
+}
+
+# Смена ядра проксирования на Mihomo
+choice_mihomo_core() {
+    command -v mihomo >/dev/null 2>&1 || { echo -e "  ${red}Ошибка${reset}: Ядро Mihomo не установлено. Выполните установку командой ${yellow}xkeen -um${reset}"; exit 1; }
+    command -v yq >/dev/null 2>&1 || { echo -e "  ${red}Ошибка${reset}: не установлен парсер конфигурационных файлов Mihomo - ${yellow}Yq${reset}"; exit 1; }
+    if grep -q 'name_client="mihomo"' $initd_dir/S24xray; then
+        echo -e " Смена ядра ${red}не выполнена${reset}. Устройство уже работает на ядре ${yellow}Mihomo${reset}"
+    elif [ -f "$install_dir/mihomo" ] && [ -f "$install_dir/yq" ] && grep -q 'name_client="xray"' $initd_dir/S24xray; then
+        if pidof "xray" >/dev/null; then
+            $initd_dir/S24xray stop
+        fi
+        sed -i 's/name_client="xray"/name_client="mihomo"/' $initd_dir/S24xray
+        add_chmod_init
+        echo -e "  ${green}Выполнена${reset} смена ядра на ${yellow}Mihomo${reset}"
+        echo -e "  Настройте конфигурацию по пути '${yellow}$mihomo_conf_dir/${reset}'"
+        echo -e "  И запустите проксирование командой ${yellow}xkeen -start${reset}"
+    else
+        echo -e " Произошла ${red}ошибка${reset} при смене ядра проксирования"
+    fi
+}
