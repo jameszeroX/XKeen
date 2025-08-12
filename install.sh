f31a1d815c42c1d80a7bd5b8a0480efa356e61ee
jameszeroX <admin@jameszero.net>
2025-08-12 14:12:54 +0300

    Init

--- install.sh
+++ install.sh
@@ -1,3 +1,12 @@
-curl -OL https://github.com/jameszeroX/XKeen/releases/latest/download/xkeen.tar.gz
+#!/bin/sh
+
+url="https://github.com/jameszeroX/XKeen/releases/latest/download/xkeen.tar.gz"
+if ! curl -OL "$url"; then
+    if ! curl -OL "https://ghfast.top/$url"; then
+        echo "Ошибка: не удалось загрузить xkeen.tar.gz"
+        exit 1
+    fi
+fi
+
 tar -xvzf xkeen.tar.gz -C /opt/sbin > /dev/null && rm xkeen.tar.gz
 xkeen -i
