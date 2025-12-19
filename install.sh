a3a3eb5ba4df5828edf7db5c33e34066cd7eab09
jameszeroX <admin@jameszero.net>
2025-12-19 08:28:08 +0300

    Update

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 url="https://github.com/jameszeroX/XKeen/releases/latest/download/xkeen.tar.gz"
-if ! curl -OL --connect-timeout 10 "$url"; then
-    if ! curl -OL --connect-timeout 10 "https://edgeone.gh-proxy.org/$url"; then
-        if ! curl -OL --connect-timeout 10 "https://ghfast.top/$url"; then
+if ! curl -OL --connect-timeout 10 -m 60 "$url"; then
+    if ! curl -OL --connect-timeout 10 -m 60 "https://edgeone.gh-proxy.org/$url"; then
+        if ! curl -OL --connect-timeout 10 -m 60 "https://ghfast.top/$url"; then
             echo "Ошибка: не удалось загрузить xkeen.tar.gz"
             exit 1
         fi
