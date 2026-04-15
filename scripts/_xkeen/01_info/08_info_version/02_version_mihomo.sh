f3b2dbcb7d417b13222ac12421f9948168156dc2
jameszeroX <admin@jameszero.net>
2026-04-15 09:19:37 +0300

    2.0 Beta

--- scripts/_xkeen/01_info/08_info_version/02_version_mihomo.sh
+++ scripts/_xkeen/01_info/08_info_version/02_version_mihomo.sh
@@ -1,12 +1,18 @@
 # Функции для получения информации о версиях Mihomo и Yq
 info_version_mihomo() {
     if [ "$mihomo_installed" = "installed" ]; then
-        mihomo_current_version=$("$install_dir/mihomo" -v 2>&1 | grep -oE 'v[0-9]+\.[0-9]+\.[0-9]+' | cut -c2-)
+        mihomo_current_version=$("$install_dir/mihomo" -v 2>&1 | grep -oE 'v?[0-9]+\.[0-9]+\.[0-9]+' | sed 's/^v//' | head -1)
+        mihomo_current_version=${mihomo_current_version:-"unknown"}
+    else
+        mihomo_current_version="unknown"
     fi
 }
 
 info_version_yq() {
     if [ "$mihomo_installed" = "installed" ]; then
-        yq_current_version=$("$install_dir/yq" -V 2>&1 | grep -oE 'v[0-9]+\.[0-9]+\.[0-9]+' | cut -c2-)
+        yq_current_version=$("$install_dir/yq" -V 2>&1 | grep -oE 'v?[0-9]+\.[0-9]+\.[0-9]+' | sed 's/^v//' | head -1)
+        yq_current_version=${yq_current_version:-"unknown"}
+    else
+        yq_current_version="unknown"
     fi
 }
--- scripts/_xkeen/01_info/08_info_version/02_version_xray.sh
+++ scripts/_xkeen/01_info/08_info_version/02_version_xray.sh
@@ -4,6 +4,9 @@ info_version_xray() {
     # Проверяем, установлен ли Xray
     if [ "$xray_installed" = "installed" ]; then
         # Если Xray установлен, получаем текущую версию
-        xray_current_version=$("$install_dir/xray" -version 2>&1 | grep -o -E 'Xray [0-9]+\.[0-9]+\.[0-9]+' | cut -d ' ' -f 2)
+        xray_current_version=$("$install_dir/xray" version 2>&1 | grep -oE 'v?[0-9]+\.[0-9]+\.[0-9]+' | sed 's/^v//' | head -1)
+        xray_current_version=${xray_current_version:-"unknown"}
+    else
+        xray_current_version="unknown"
     fi
 }
