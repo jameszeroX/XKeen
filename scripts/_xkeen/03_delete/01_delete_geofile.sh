ec37049287b7f882a3ec88875716a339e488f08a
jameszeroX <admin@jameszero.net>
2026-09-13 19:46:50 +0300

    refactor: 01_delete_geofile.sh

--- scripts/_xkeen/03_delete/01_delete_geofile.sh
+++ scripts/_xkeen/03_delete/01_delete_geofile.sh
@@ -2,7 +2,7 @@
 delete_geosite() {
     [ "$choice_delete_geosite_refilter_select" = "true" ] && rm -f "$geo_dir/geosite_refilter.dat"
     [ "$choice_delete_geosite_v2fly_select" = "true" ] && rm -f "$geo_dir/geosite_v2fly.dat"
-    [ "$choice_delete_geosite_zkeen_select" = "true" ] && rm -f "$geo_dir/"geosite_zkeen.dat "$geo_dir/"zkeen.dat
+    [ "$choice_delete_geosite_zkeen_select" = "true" ] && rm -f "$geo_dir/geosite_zkeen.dat" "$geo_dir/zkeen.dat"
 }
 
 # Функция для удаления всех файлов GeoSite
