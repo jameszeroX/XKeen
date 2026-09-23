c61239f278c430b2d439e8ddcaa5692674d82292
jameszeroX <admin@jameszero.net>
2026-09-23 08:18:36 +0300

    fix: сузить regex детекции geofile-cron задачи в общем crontab root

--- scripts/_xkeen/03_delete/03_delete_cron.sh
+++ scripts/_xkeen/03_delete/03_delete_cron.sh
@@ -3,8 +3,6 @@ delete_cron_geofile() {
     if [ -f "$cron_dir/$cron_file" ]; then
         tmp_file="$cron_dir/${cron_file}.tmp"
         cp "$cron_dir/$cron_file" "$tmp_file"
-        write_tmp="$cron_dir/${cron_file}.tmp.$$"
-        grep -v "xkeen.*-ug" "$tmp_file" | grep -v '^[[:space:]]*$' > "$write_tmp"
-        mv -f "$write_tmp" "$cron_dir/$cron_file" || rm -f "$write_tmp"
+        grep -v "xkeen.*-ug" "$tmp_file" | grep -v '^\s*$' > "$cron_dir/$cron_file"
     fi
 }
